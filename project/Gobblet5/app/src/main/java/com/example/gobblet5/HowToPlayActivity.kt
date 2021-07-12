e6934f88c259c021d18b3bc059450092c9ddd38d
知念英弥 <hideya670@gmail.com>
2021-07-12 15:56:06 +0900

    チュートリアルの挙動を確認 一応動きはした

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToPlayActivity.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToPlayActivity.kt
@@ -1,6 +1,7 @@
 package com.example.gobblet5
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_how_to_play.*
 
@@ -12,11 +13,6 @@ class HowToPlayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_how_to_play)
-
-    }
-
-    override fun onStart() {
-        super.onStart()
         //bundle宣言,保存
         val bundle=Bundle()
         bundle.putString("SampleText",tutorialText)
@@ -28,6 +24,44 @@ class HowToPlayActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.TextContainer,textFragment)
             .addToBackStack(null)
             .commit()
+
+        fun changeFragment(){
+            when(page){
+                1->bundle.putString("SampleText","page1")
+                2->bundle.putString("SampleText","page2")
+                3->bundle.putString("SampleText","page3")
+                4->bundle.putString("SampleText","page4")
+                5->bundle.putString("SampleText","page5")
+            }
+            val textFragment = TutorialTextFragment()
+            textFragment.arguments=bundle//フラグメントにargumentsをわたす
+            val fragmentManager = this.supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.TextContainer,textFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        preButton.setOnClickListener {
+            if (page>0){
+                page-=1
+                Log.d("gobblet2","page:${page}")
+                changeFragment()
+            }
+        }
+
+        nextButton.setOnClickListener {
+            if (page<5){
+                page+=1
+                Log.d("gobblet2","page:${page}")
+                changeFragment()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
     }
 
 
