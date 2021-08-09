ecba72cd0882ebb00415ebcfbf42dd30f28bc61f
知念英弥 <hideya670@gmail.com>
2021-08-09 20:52:48 +0900

    色々しくじった

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/debActivity.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/debActivity.kt
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_deb.*
-import kotlinx.android.synthetic.main.fragment_how_to_play2.*
 
 
 class debActivity : AppCompatActivity() {
@@ -18,28 +16,15 @@ class debActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deb)
 
-        startHowToPlay2Timer()
     }
 
     fun startHowToPlay2Timer(){
         val handler = Handler()
         val timer = object :Runnable{
             override fun run() {
-                when(time){
-                    1000L -> {
-                        Log.d("gobblet2", "call")
-                        val fragment = HowToPlayFragment2()
-                        val fragmentManager = supportFragmentManager
-                        val fragmentTransaction  = fragmentManager.beginTransaction()
-                        fragment.visiblSmall()
-                        fragmentTransaction.replace(R.id.fragmentBox,fragment)
-                            .addToBackStack(null)
-                            .commit()
-                    }
-                }
                 time += millisecond
                 handler.postDelayed(this,millisecond)
-                Log.d("gobblet2", "timer_def:${time}")
+                Log.d("gobbl et2", "timer_def:${time}")
                 if (time==3000L){
                     handler.removeCallbacks(this)
                     time = 0L
