ad98170abcc1181157ed55cf7d5bb4152dbdf44c
知念英弥 <hideya670@gmail.com>
2021-08-10 19:35:45 +0900

    たくさん作ったよ

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToplayFragment8_1.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToplayFragment8_1.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HowToPlayFragment7.newInstance] factory method to
+ * Use the [HowToplayFragment8_1.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HowToPlayFragment7 : Fragment() {
+class HowToplayFragment8_1 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class HowToPlayFragment7 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_how_to_play7, container, false)
+        return inflater.inflate(R.layout.fragment_how_toplay_8_1fragment, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class HowToPlayFragment7 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HowToPlayFragment7.
+         * @return A new instance of fragment HowToplayFragment8_1.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HowToPlayFragment7().apply {
+            HowToplayFragment8_1().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
