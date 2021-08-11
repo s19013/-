ebb66224e5b2a269787f4bb3fac2846de065952f
知念英弥 <hideya670@gmail.com>
2021-08-11 16:33:22 +0900

    修正

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToPlayFragment5.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToPlayFragment5.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HowToPlay5Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HowToPlay5Fragment : Fragment() {
+class HowToPlayFragment5 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,23 +37,5 @@ class HowToPlay5Fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_how_to_play5, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HowToPlay5Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HowToPlay5Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
--- project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToPlayFragment7_1.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToPlayFragment7_1.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HowToPlayFragment7.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HowToPlayFragment7 : Fragment() {
+class HowToPlayFragment7_1 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,23 +37,5 @@ class HowToPlayFragment7 : Fragment() {
         return inflater.inflate(R.layout.fragment_how_to_play7_1, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HowToPlayFragment7.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HowToPlayFragment7().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
