1013f14ba94154c6666616468d16aaff0e11dd9a
s20019 <s20019@std.it-college.ac.jp>
2021-08-12 12:41:08 +0900

    チュートリアル選択画面、操作説明画面作成

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToControlActivity.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/HowToControlActivity.kt
@@ -2,10 +2,74 @@ package com.example.gobblet5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_how_to_control.*
 
 class HowToControlActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_how_to_control)
+
+        val preButton = findViewById<Button>(R.id.preButton)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        val backButton = findViewById<Button>(R.id.backButton)
+
+
+        preButton.setOnClickListener { countDownPage() }
+
+        nextButton.setOnClickListener { countUpPage() }
+
+        backButton.setOnClickListener { finish() }
     }
+
+    val maxPage = 10
+    var page: Int = 1
+
+
+    private fun countUpPage() {
+        page += 1
+        if (page > maxPage) {
+            changeText()
+            changeImg()
+            changeCurrentPage()
+        }
+        else {
+            page = maxPage
+            changeText()
+            changeImg()
+            changeCurrentPage()
+        }
+    }
+
+    private fun countDownPage() {
+        page -= 1
+        if (page < 1) {
+            changeText()
+            changeImg()
+            changeCurrentPage()
+        }
+        else {
+            page = 1
+            changeText()
+            changeImg()
+            changeCurrentPage()
+        }
+    }
+
+    private fun changeText() {
+        when {
+            page == 1 -> tutorialText.text = getString(R.string.HowToOperateText)  // HowToOperateText1
+            page == 2 -> tutorialText.text = getString(R.string.HowToOperateText)  // HowToOperateText2
+            page == 3 -> tutorialText.text = getString(R.string.HowToOperateText)  // HowToOperateText3
+        }
+    }
+
+
+    private fun changeImg() { }
+
+
+    private fun changeCurrentPage() {
+        currentPageText.text = page.toString()
+    }
+
 }
