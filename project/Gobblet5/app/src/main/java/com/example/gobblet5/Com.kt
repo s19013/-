afbd3b1e69fcef1ce117d0b3aad025b7bcf1a01d
知念英弥 <s19013@std.it-college.ac.jp>
2021-10-18 15:32:47 +0900

    なんかとにかく色々やりすぎた とりあえず自分のリーチを把握させることができたと思う

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/Com.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/Com.kt
@@ -24,16 +24,18 @@ class Com {
     private var temochiBig:Temochi? = null
     private var temochiMiddle:Temochi? = null
     private var temochiSmall:Temochi? = null
-    private var line1:MutableList<Mas> = mutableListOf()
-    private var line2:MutableList<Mas> = mutableListOf()
-    private var line3:MutableList<Mas> = mutableListOf()
-    private var line4:MutableList<Mas> = mutableListOf()
-    private var lineA:MutableList<Mas> = mutableListOf()
-    private var lineB:MutableList<Mas> = mutableListOf()
-    private var lineC:MutableList<Mas> = mutableListOf()
-    private var lineD:MutableList<Mas> = mutableListOf()
-    private var lineS:MutableList<Mas> = mutableListOf()
-    private var lineBS:MutableList<Mas> = mutableListOf()
+    private var line1:Line = Line(stringLine1)
+    private var line2:Line = Line(stringLine2)
+    private var line3:Line = Line(stringLine3)
+    private var line4:Line = Line(stringLine4)
+    private var lineA:Line = Line(stringLineA)
+    private var lineB:Line = Line(stringLineB)
+    private var lineC:Line = Line(stringLineC)
+    private var lineD:Line = Line(stringLineD)
+    private var lineS:Line = Line(stringLineS)
+    private var lineBS:Line = Line(stringLineBS)
+
+
     private var doNotMove:MutableList<String> = mutableListOf()
     private var EnemyReachList:MutableList<String> = mutableListOf()
     private var ComReachList:MutableList<String> = mutableListOf()
@@ -51,49 +53,72 @@ class Com {
 //    )
     //これでリーチになりそうなものをしらべる?(マップは使えないのでリストを使う)
 
-
-
-
-    fun iniLines(line1:MutableList<Mas>,line2:MutableList<Mas>,line3:MutableList<Mas>,line4:MutableList<Mas>,
-                 lineA:MutableList<Mas>,lineB:MutableList<Mas>,lineC:MutableList<Mas>,lineD:MutableList<Mas>,
-                 lineS:MutableList<Mas>,lineBS:MutableList<Mas>){
-        this.line1 = line1
-        this.line2 = line2
-        this.line3 = line3
-        this.line4 = line4
-        this.lineA = lineA
-        this.lineB = lineB
-        this.lineC = lineC
-        this.lineD = lineD
-        this.lineS = lineS
-        this.lineBS = lineBS
-
-//        Log.d("gobblet2Com","list1[0]:${line1[0].nameGetter()}")
-    }
-
-
     //マスが空かどうかしらべる
-    fun checkEmptyMas(){
-        for (L in lineAllAtOnce){
-            if (L.returnLastElement() == 0){
-                L.scoreAdd(10) //マスの中が空だったら評価値10を加える
+    fun checkEmptyMas() {
+        fun commonFunc(line: Line){
+            for (L in line.list) {
+                if (L.returnLastElement() == 0) {
+                    L.addScore(10) //マスの中が空だったら評価値10を加える
+                }
             }
         }
+        commonFunc(line1)
+        commonFunc(line2)
+        commonFunc(line3)
+        commonFunc(line4)
     }
 
     //各マスに何が入っているのかしらべて評価値をつける
     fun checkWhatIsInTheMas(){
-        for (L in lineAllAtOnce){
-            if (L.returnLastElement() == 0) {continue}
-            val rv= L.funcForDisplay() //帰り値
-            when{
-                rv[0] == 3 ->{L.scoreAdd(-50)} //相手の大コマか自分の大コマが置かれている
-                rv[1] == 1 && rv[0] == 2 -> {L.scoreAdd(-32)} //相手の中コマが置かれている
-                rv[1] == 1 && rv[0] == 1 -> {L.scoreAdd(-11)} //相手の小コマが置かれている
-                rv[1] == -1 && rv[0] == 2 -> {L.scoreAdd(-8)} //自分の中コマが置かれている
-                rv[1] == -1 && rv[0] == 1 -> {L.scoreAdd(-9)} //自分の小コマが置かれている
+        fun commonFunc(line: Line){
+            for (mas in line.list){
+                val rv = mas.funcForDisplay() //帰り値を入れる箱を用意する
+                when{
+                    rv[0] == 3 -> {mas.addScore(-50)}//相手の大コマか自分の大コマが置かれている
+                    rv[0] == 2 && rv[1] ==1 ->{mas.addScore(-38)}//相手の中コマが置かれている
+                    rv[0] == 1 && rv[1] ==1 ->{mas.addScore(-19)}//相手の小コマが置かれている
+                    rv[0] == 2 && rv[1] ==-1 ->{mas.addScore(-8)}//自分の中コマが置かれている
+                    rv[0] == 1 && rv[1] ==-1 ->{mas.addScore(-9)}//自分の小コマが置かれている
+                }
             }
         }
+
+        commonFunc(line1)
+        commonFunc(line2)
+        commonFunc(line3)
+        commonFunc(line4)
+    }
+
+    //リーチなった列がないか調べる
+    fun reachChecker(){
+        fun counter(line: Line){
+            var countM1 = 0
+            var countP1 = 0
+            for (i in line.list){ // -1,1の個数をそれぞれ数える
+                when(i.returnLastElement()) {
+                    -1 -> {countM1 += 1}
+                    1 -> {countP1 += 1}
+                }
+                if (countP1 >=3){
+                    EnemyReachList.add(line.nameGetter())
+                    return
+                } //1が3つ以上なら敵がリーチ
+                if (countM1 >=3){
+                    ComReachList.add(line.nameGetter())
+                    return
+                } //-1 が3つ以上ならcomがリーチ
+            }
+        }
+        counter(line1)
+        counter(line2)
+        counter(line3)
+        counter(line4)
+        counter(lineA)
+        counter(lineB)
+        counter(lineC)
+        counter(lineD)
+        counter(lineS)
+        counter(lineBS)
     }
 
     //人間にリーチがかかってないか調べる
@@ -109,63 +134,99 @@ class Com {
     fun checkComReach(){
         for(value in 0 until judgeList.size){
             if (judgeList[value] == -3){
-                ComReachList.add(nameList[value])
+                //ComReachList.add(nameList[value])
             }
         }
     }
 
+    //止めをさせる場所を探す
     fun checkmate(){
+        //最後の決めてとなる場所を探す,そしてそこに入れられるかを探す
+        fun commonFunc(line:Line){ 
+            Log.d("gobblet2Com","line:${line.nameGetter()}")
+            var finalTarget:Mas? = null
+            //ここからどのマスがまだ自分のマスでないかを教える?
+            for (i in line.list){
+                if (i.returnLastElement() != -1){
+                    finalTarget=i
+                    //あとは大きさがわかれば良い
+                    break
+                }
+            }
+
+            //コマをおけば勝てるところに相手の大きいコマがおいてないか調べる
+            if (finalTarget != null){
+                when(howBigEnemysPiece(finalTarget)){
+                    3 -> {finalTarget.addScore(-100)}//諦めること指す
+                    else -> {finalTarget.addScore(200)}//評価値を入れる
+                }
+            }
+            Log.d("gobblet2Com","finalTarget:${finalTarget?.nameGetter()}")
+        }
+        
         for (value in ComReachList){
-            Log.d("gobblet2Com","${value}")
             when(value){
-                stringLine1 ->  {searchFinalTarget(bord[0][0],bord[0][1],bord[0][2],bord[0][3])}
-                stringLine2 ->  {searchFinalTarget(bord[1][0],bord[1][1],bord[1][2],bord[1][3])}
-                stringLine3 ->  {searchFinalTarget(bord[2][0],bord[2][1],bord[2][2],bord[2][3])}
-                stringLine4 ->  {searchFinalTarget(bord[3][0],bord[3][1],bord[3][2],bord[3][3])}
-                stringLineA ->  {searchFinalTarget(bord[0][0],bord[1][0],bord[2][0],bord[3][0])}
-                stringLineB ->  {searchFinalTarget(bord[0][1],bord[1][1],bord[2][1],bord[3][1])}
-                stringLineC ->  {searchFinalTarget(bord[0][2],bord[1][2],bord[2][2],bord[3][2])}
-                stringLineD ->  {searchFinalTarget(bord[0][3],bord[1][3],bord[2][3],bord[3][3])}
-                stringLineS ->  {searchFinalTarget(bord[0][3],bord[1][2],bord[2][1],bord[3][0])}
-                stringLineBS -> {searchFinalTarget(bord[0][0],bord[1][1],bord[2][2],bord[3][3])}
+                 stringLine1 -> {commonFunc(line1)}
+                 stringLine2 -> {commonFunc(line2)}
+                 stringLine3 -> {commonFunc(line3)}
+                 stringLine4 -> {commonFunc(line4)}
+                 stringLineA -> {commonFunc(lineA)}
+                 stringLineB -> {commonFunc(lineB)}
+                 stringLineC -> {commonFunc(lineC)}
+                 stringLineD -> {commonFunc(lineD)}
+                 stringLineS -> {commonFunc(lineS)}
+                stringLineBS -> {commonFunc(lineBS)}
             }
         }
-        debScore()
     }
 
-    //最後の決めてとなる場所を探す,そしてそこに入れられるかを探す
-    fun searchFinalTarget(mas1: Mas,mas2: Mas,mas3: Mas,mas4: Mas){
-        val box = listOf<Mas>(mas1,mas2,mas3,mas4)
-        Log.d("gobblet2Com","[${mas1.nameGetter()},${mas2.nameGetter()},${mas3.nameGetter()},${mas4.nameGetter()}]")
-        var finalTarget:Mas? = mas1
-        //ここからどのマスがまだ自分のマスでないかを教える?
-        for (i in box){
-            if (i.returnLastElement() != -1){
-                finalTarget=i
-                //あとは大きさがわかれば良い
-
-                break
-            }
-        }
-
-        if (finalTarget != null){
-            when(howBigEnemysPiece(finalTarget)){
-                3 -> {finalTarget.scoreAdd(-100)}//諦めること指す
-                else -> {finalTarget.scoreAdd(100)}//評価値を入れる
-            }
-        }
-        Log.d("gobblet2Com","finalTarget:${finalTarget?.nameGetter()}")
-    }
-
-
+    //敵のコマの大きさを調べる
     fun howBigEnemysPiece(mas:Mas):Int{
         val rv = mas.funcForDisplay()
         return rv[0]
     }
 
+    //相手の勝利を阻止する
     fun blockCheckmate(){
+        ////どこに入れれば防げるか探す
+        fun commonFunc(line:Line){
+            Log.d("gobblet2Com","line:${line.nameGetter()}")
+            var finalTarget:Mas? = null
+            //ここからどのマスがまだ自分のマスでないかを教える?
+            for (i in line.list){
+                if (i.returnLastElement() != -1){
+                    finalTarget=i
+                    //あとは大きさがわかれば良い
+                    break
+                }
+            }
 
+            if (finalTarget != null){//コマをおけば勝てるところに相手の大きいコマがおいてないか調べる
+                when(howBigEnemysPiece(finalTarget)){
+                    3 -> {finalTarget.addScore(-100)}//諦めること指す
+                    else -> {finalTarget.addScore(200)}//評価値を入れる
+                }
+            }
+            Log.d("gobblet2Com","finalTarget:${finalTarget?.nameGetter()}")
+        }
+        for (value in ComReachList){
+            when(value){
+                stringLine1 -> {commonFunc(line1)}
+                stringLine2 -> {commonFunc(line2)}
+                stringLine3 -> {commonFunc(line3)}
+                stringLine4 -> {commonFunc(line4)}
+                stringLineA -> {commonFunc(lineA)}
+                stringLineB -> {commonFunc(lineB)}
+                stringLineC -> {commonFunc(lineC)}
+                stringLineD -> {commonFunc(lineD)}
+                stringLineS -> {commonFunc(lineS)}
+                stringLineBS -> {commonFunc(lineBS)}
+            }
+        }
     }
+
+
+    
 
     fun judge(){
         for(i in 0..3){
@@ -200,28 +261,21 @@ class Com {
         }
     }
 
-
-
-    //
-
     fun resetScore(){
         //すべてのマスクラスの評価値を0にする
-        for (L in lineAllAtOnce){
-            L.resetScore()
+        for (l in line1.list){
+            l.resetScore()
         }
-    }
+        for (l in line2.list){
+            l.resetScore()
+        }
+        for (l in line3.list){
+            l.resetScore()
+        }
+        for (l in line4.list){
+            l.resetScore()
+        }
 
-    fun iniConcatLine(){ //一旦関数にしないとエラーになるので関数化
-        lineAllAtOnce.addAll(line1)
-        lineAllAtOnce.addAll(line2)
-        lineAllAtOnce.addAll(line3)
-        lineAllAtOnce.addAll(line4)
-
-        //
-        bord.add(line1)
-        bord.add(line2)
-        bord.add(line3)
-        bord.add(line4)
     }
 
     fun resetLists(){
@@ -232,23 +286,52 @@ class Com {
         ComReachList.clear()
     }
 
+    fun iniConcatLine(){ //一旦関数にしないとエラーになるので関数化
+//        lineAllAtOnce.addAll(line1)
+//        lineAllAtOnce.addAll(line2)
+//        lineAllAtOnce.addAll(line3)
+//        lineAllAtOnce.addAll(line4)
+
+        //
+        bord.add(line1.list)
+        bord.add(line2.list)
+        bord.add(line3.list)
+        bord.add(line4.list)
+    }
+
+    fun iniLines(line1:MutableList<Mas>,line2:MutableList<Mas>,line3:MutableList<Mas>,line4:MutableList<Mas>,
+                 lineA:MutableList<Mas>,lineB:MutableList<Mas>,lineC:MutableList<Mas>,lineD:MutableList<Mas>,
+                 lineS:MutableList<Mas>,lineBS:MutableList<Mas>){
+        this.line1.listSetter(line1)
+        this.line2.listSetter(line2)
+        this.line3.listSetter(line3)
+        this.line4.listSetter(line4)
+        this.lineA.listSetter(lineA)
+        this.lineB.listSetter(lineB)
+        this.lineC.listSetter(lineC)
+        this.lineD.listSetter(lineD)
+        this.lineS.listSetter(lineS)
+        this.lineBS.listSetter(lineBS)
+    }
+
     fun deb(){
-        Log.d("gobblet2Com","list1:{${line1[0].funcForDisplay()},${line1[1].funcForDisplay()},${line1[2].funcForDisplay()},${line1[3].funcForDisplay()}}")
-        Log.d("gobblet2Com","list2:{${line2[0].funcForDisplay()},${line2[1].funcForDisplay()},${line2[2].funcForDisplay()},${line2[3].funcForDisplay()}}")
-        Log.d("gobblet2Com","list3:{${line3[0].funcForDisplay()},${line3[1].funcForDisplay()},${line3[2].funcForDisplay()},${line3[3].funcForDisplay()}}")
-        Log.d("gobblet2Com","list4:{${line4[0].funcForDisplay()},${line4[1].funcForDisplay()},${line4[2].funcForDisplay()},${line4[3].funcForDisplay()}}")
-        Log.d("gobblet2Com","\n")
+        Log.d("gobblet2Com","list1:{${bord[0][0].funcForDisplay()},${bord[0][1].funcForDisplay()},${bord[0][2].funcForDisplay()},${bord[0][0].funcForDisplay()}}")
+        Log.d("gobblet2Com","list2:{${bord[1][0].funcForDisplay()},${bord[1][1].funcForDisplay()},${bord[1][2].funcForDisplay()},${bord[1][3].funcForDisplay()}}")
+        Log.d("gobblet2Com","list3:{${bord[2][0].funcForDisplay()},${bord[2][1].funcForDisplay()},${bord[2][2].funcForDisplay()},${bord[2][3].funcForDisplay()}}")
+        Log.d("gobblet2Com","list4:{${bord[3][0].funcForDisplay()},${bord[3][1].funcForDisplay()},${bord[3][2].funcForDisplay()},${bord[3][1].funcForDisplay()}}")
+        Log.d("gobblet2Com"," ")
     }
 
     fun debScore(){
-        Log.d("gobblet2Com","list1:{${line1[0].scoreGetter()},${line1[1].scoreGetter()},${line1[2].scoreGetter()},${line1[3].scoreGetter()}}")
-        Log.d("gobblet2Com","list2:{${line2[0].scoreGetter()},${line2[1].scoreGetter()},${line2[2].scoreGetter()},${line2[3].scoreGetter()}}")
-        Log.d("gobblet2Com","list3:{${line3[0].scoreGetter()},${line3[1].scoreGetter()},${line3[2].scoreGetter()},${line3[3].scoreGetter()}}")
-        Log.d("gobblet2Com","list4:{${line4[0].scoreGetter()},${line4[1].scoreGetter()},${line4[2].scoreGetter()},${line4[3].scoreGetter()}}")
-        Log.d("gobblet2Com","\n")
-        Log.d("gobblet2Com","${judgeList}")
+        Log.d("gobblet2Com","list1:{${bord[0][0].scoreGetter()},${bord[0][1].scoreGetter()},${bord[0][2].scoreGetter()},${bord[0][3].scoreGetter()}}")
+        Log.d("gobblet2Com","list2:{${bord[1][0].scoreGetter()},${bord[1][1].scoreGetter()},${bord[1][2].scoreGetter()},${bord[1][3].scoreGetter()}}")
+        Log.d("gobblet2Com","list3:{${bord[2][0].scoreGetter()},${bord[2][1].scoreGetter()},${bord[2][2].scoreGetter()},${bord[2][3].scoreGetter()}}")
+        Log.d("gobblet2Com","list4:{${bord[3][0].scoreGetter()},${bord[3][1].scoreGetter()},${bord[3][2].scoreGetter()},${bord[3][3].scoreGetter()}}")
+        Log.d("gobblet2Com"," ")
+        Log.d("gobblet2Com","judgeList:${judgeList}")
         Log.d("gobblet2Com","ComReachList:${ComReachList}")
         Log.d("gobblet2Com","EnemyReachList:${EnemyReachList}")
+        Log.d("gobblet2Com"," ")
     }
 
     fun debBord(){
