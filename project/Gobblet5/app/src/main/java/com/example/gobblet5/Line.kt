fd2a56d32c28bf5ca0a036836b8f22882e913336
知念英弥 <s19013@std.it-college.ac.jp>
2021-10-21 16:54:56 +0900

    リストを取得する関数を作った

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/Line.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/Line.kt
@@ -13,6 +13,10 @@ class Line(private val name:String) {
         this.list = mutableList
     }
 
+    fun listGetter():MutableList<Mas>{
+        return list
+    }
+
     fun deb(){
         Log.d("gobblet2Com","${name}:${list[0].nameGetter()},${list[1].nameGetter()},${list[2].nameGetter()},${list[3].nameGetter()}")
 
