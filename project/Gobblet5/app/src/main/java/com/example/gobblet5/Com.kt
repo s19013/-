452d259539662c788a4b1ef13f55bd578275b669
知念英弥 <s19013@std.it-college.ac.jp>
2021-10-26 16:56:12 +0900

    修正

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/Com.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/Com.kt
@@ -9,26 +9,18 @@ import java.util.stream.Stream
 
 class Com {
     //マジックナンバー防止
-    private val stringLineA="LA"
-    private val stringLineB="LB"
-    private val stringLineC="LC"
-    private val stringLineD="LD"
     private val stringLine1="L1"
     private val stringLine2="L2"
     private val stringLine3="L3"
     private val stringLine4="L4"
+    private val stringLineA="LA"
+    private val stringLineB="LB"
+    private val stringLineC="LC"
+    private val stringLineD="LD"
     private val stringLineS="LS"
     private val stringLineBS="LBS"
 
-    //
-    var destination :Mas? = null //どこに置くか決めた
-    var movingSource:String? = null
-    private var turnCount = 0 //自分のターンが回ってきた回数
-    private var isFirstTurn = true
-    private var P1Count = 0
-    private var temochiBig:Temochi? = null
-    private var temochiMiddle:Temochi? = null
-    private var temochiSmall:Temochi? = null
+    //ライン
     private val line1:Line = Line(stringLine1)
     private val line2:Line = Line(stringLine2)
     private val line3:Line = Line(stringLine3)
@@ -40,18 +32,39 @@ class Com {
     private val lineS:Line = Line(stringLineS)
     private val lineBS:Line = Line(stringLineBS)
 
+    //手持ち
+    private var temochiBig:Temochi? = null
+    private var temochiMiddle:Temochi? = null
+    private var temochiSmall:Temochi? = null
+
+    //
+    var destination :Mas? = null //移動先
+    var movingSource:String? = null //移動元
+    var size = 0 //強制的に動かすコマの大きさを決める時に使う
+
+    private var turnCount = 0 //自分のターンが回ってきた回数
+
+
+
+    //考えるのに使う道具?
+    private var masInTheBigPiece:MutableList<Mas> = mutableListOf()   //
+    private var masInTheMiddlePiece:MutableList<Mas> = mutableListOf()//
+    private var masInTheSmallPiece:MutableList<Mas> = mutableListOf() //
 
     private var doNotMoveList:MutableList<Mas> = mutableListOf() //動かしては行けないコマを管理
     private var unnecessaryList:MutableList<String> = mutableListOf() //うごかしても問題ないコマを管理
     private var candidateList:MutableList<Mas> = mutableListOf() //コマを入れる候補を管理するリスト
-    private var enemyReachList:MutableList<String> = mutableListOf() //敵にリーチがかかっているラインを管理するリスト
-    private var comReachList:MutableList<String> = mutableListOf() //自分にリーチがかかっているラインを管理するリスト
+    private var enemyReachList:MutableList<Line> = mutableListOf() //敵にリーチがかかっているラインを管理するリスト
+    private var comReachList:MutableList<Line> = mutableListOf() //自分にリーチがかかっているラインを管理するリスト
     private var bord:MutableList<MutableList<Mas>> = mutableListOf() //[縦列][横列]　例:B3 -> [2][1]
     private var lineAllAtOnce:MutableList<Line> = mutableListOf() //すべてのラインクラスに対して色々やる時に使うリスト
     private var judgeList:MutableList<Int> = mutableListOf(0,0,0,0,0,0,0,0,0,0)
     private var nameList:MutableList<String> = mutableListOf(stringLine1,stringLine2,stringLine3,stringLine4,
         stringLineA,stringLineB,stringLineC,stringLineD,stringLineS,stringLineBS)
 
+    //デバッグ用
+    private var debComReachList = mutableListOf<String>()
+    private var debEnemyReachList = mutableListOf<String>()
     private var debCandidateList= mutableListOf<String>()
     private var debDoNotMoveList= mutableListOf<String>()
 
@@ -75,17 +88,17 @@ class Com {
             for (mas in thisLine){
                 if (mas == standard) {continue} //基準のマスを調べようとしたらスキップ
                 if (mas.returnLastElement() == 1){ countP1 +=1 }
-                if (countP1 == 3){doNotMoveList.add(standard!!) }
+                if (countP1 == 3){doNotMoveList.add(standard!!) } //ライン上は自分以外全部敵のコマだったらそのコマを動かさないリストに追加
             }
         }
 
 
         //基準のマスに相手のコマが入っていた､もしくは何も入ってなかった場合
         fun standardIsP1(){
+            if (standard!!.funcForDisplay()[0] == 3) {return}
             for (mas in thisLine){
                 val rv = mas.funcForDisplay()
                 if (mas == standard) {continue} //基準のマスを調べようとしたらスキップ
-                if (standard!!.funcForDisplay()[0] == 3) {continue}
 
                 //周りの各マスを調べて
                 //基準にしたマスに評価値を入れる
@@ -99,8 +112,7 @@ class Com {
 
         for (i in 0..3){
             standard=line.listGetter()[i]
-            //そのラインの中での基準を決める
-            //基準はA1,B1,C1と変わって行く
+            //ライン上で一番前のマスから順に基準のマスにしていく
 
             //基準となったマスに何が入っているかによってすることが違う
             when(standard.funcForDisplay()[1]){
@@ -113,7 +125,8 @@ class Com {
     //Log.d("gobblet2Com","${bord[y][x].nameGetter()}に緑")
     //Log.d("gobblet2Com","${bord[y][x].nameGetter()}に赤")
     }
-    
+
+    //周りををしらべている時に空のコマがあった時の処理
     fun inTheCaseOfEmp(mas:Mas){
         mas.addScore(20)
 //        Log.d("gobblet2Com","${mas.nameGetter()} add:10")
@@ -133,7 +146,6 @@ class Com {
 
     //周りををしらべている時に相手のコマがあった時の処理
     fun inTheCaseOfP1(size:Int,mas: Mas){
-        P1Count+=1
         when(size){
             1->{ mas.addScore(-20) } //小
             2->{ mas.addScore(-40) } //中
@@ -161,6 +173,7 @@ class Com {
     }
 
     //各マスに何が入っているのかしらべて評価値をつける
+    //ついでにコンピューターのコマがどこにあるかも調べる
     fun checkWhatIsInTheMas(){
         fun commonFunc(line: Line){
             for (mas in line.listGetter()){
@@ -191,11 +204,11 @@ class Com {
                     1 -> {countP1 += 1}
                 }
                 if (countP1 >=3){
-                    enemyReachList.add(line.nameGetter())
+                    enemyReachList.add(line)
                     return
                 } //1が3つ以上なら敵がリーチ
                 if (countM1 >=3){
-                    comReachList.add(line.nameGetter())
+                    comReachList.add(line)
                     return
                 } //-1 が3つ以上ならcomがリーチ
             }
@@ -228,23 +241,12 @@ class Com {
                     0 -> {finalTarget.addScore(1000)}//空だった
                 }
             }
-//            destination = finalTarget
+            //destination = finalTarget
             Log.d("gobblet2Com","finalTarget:${finalTarget?.nameGetter()}")
         }
         
         for (value in comReachList){
-            when(value){
-                 stringLine1 -> {commonFunc(line1)}
-                 stringLine2 -> {commonFunc(line2)}
-                 stringLine3 -> {commonFunc(line3)}
-                 stringLine4 -> {commonFunc(line4)}
-                 stringLineA -> {commonFunc(lineA)}
-                 stringLineB -> {commonFunc(lineB)}
-                 stringLineC -> {commonFunc(lineC)}
-                 stringLineD -> {commonFunc(lineD)}
-                 stringLineS -> {commonFunc(lineS)}
-                stringLineBS -> {commonFunc(lineBS)}
-            }
+            commonFunc(value)
         }
     }
 
@@ -274,18 +276,7 @@ class Com {
             Log.d("gobblet2Com","blocktarget:${target?.nameGetter()}")
         }
         for (value in enemyReachList){
-            when(value){
-                stringLine1 -> {commonFunc(line1)}
-                stringLine2 -> {commonFunc(line2)}
-                stringLine3 -> {commonFunc(line3)}
-                stringLine4 -> {commonFunc(line4)}
-                stringLineA -> {commonFunc(lineA)}
-                stringLineB -> {commonFunc(lineB)}
-                stringLineC -> {commonFunc(lineC)}
-                stringLineD -> {commonFunc(lineD)}
-                stringLineS -> {commonFunc(lineS)}
-                stringLineBS -> {commonFunc(lineBS)}
-            }
+            commonFunc(value)
         }
     }
 
@@ -300,24 +291,24 @@ class Com {
         when(randomNum){
             0 ->{ destination=lineB.listGetter()[1] } //b2に置く
             1 ->{ destination=lineB.listGetter()[2] } //b3に置く
-            2 ->{ destination=lineC.listGetter()[1]} //c2に置く
-            3 ->{ destination=lineC.listGetter()[2]} //c3に置く
+            2 ->{ destination=lineC.listGetter()[1] } //c2に置く
+            3 ->{ destination=lineC.listGetter()[2] } //c3に置く
         }
         movingSource= temochiBig?.nameGetter()
     }
 
     //2ターン目の定石
     fun secondTurn(){
-
+        movingSource=temochiBig?.nameGetter()
     }
 
     //大きさを決定する
-    fun ChooseASize(){
+    fun chooseASize(){
 
     }
 
     //場所を決める
-    fun ChooseAlocation(){
+    fun chooseAlocation(){
 
     }
 
@@ -341,15 +332,22 @@ class Com {
             ChoosePickup()
             return
         }
+        //1ターン目
         if (turnCount==1){
             firstTurn()
             return
         }
+        //2ターン目
         if (turnCount==2){
-
+            secondTurn()
+            return
         }
 
 
+
+    }
+
+    fun standardProcessing(){
 
     }
 
@@ -502,7 +500,7 @@ class Com {
         Log.d("gobblet2Com","list4:{${bord[3][0].scoreGetter()},${bord[3][1].scoreGetter()},${bord[3][2].scoreGetter()},${bord[3][3].scoreGetter()}}")
         Log.d("gobblet2Com"," ")
         Log.d("gobblet2Com","judgeList:${judgeList}")
-        Log.d("gobblet2Com","comReachList:${comReachList}")
+        Log.d("gobblet2Com","comReachList:${debComReachList}")
         Log.d("gobblet2Com","enemyReachList:${enemyReachList}")
         debC()
         Log.d("gobblet2Com","candidateList:${debCandidateList}")
@@ -519,6 +517,15 @@ class Com {
         for (i in doNotMoveList){
             debDoNotMoveList.add(i.nameGetter())
         }
+        debComReachList.clear()
+        for (i in comReachList){
+            debComReachList.add(i.nameGetter())
+        }
+        debEnemyReachList.clear()
+        for (i in enemyReachList){
+            debEnemyReachList.add(i.nameGetter())
+        }
+        
     }
 
     fun debBord(){
