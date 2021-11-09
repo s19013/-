1ded99c8829f5fe405faf737fbd76cd61ba2ec50
知念英弥 <s19013@std.it-college.ac.jp>
2021-11-09 15:00:48 +0900

    #整頓整頓 #追加
    同じライン上で自分の大きいコマが3つ使われているかを調べる関数などを追加

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/Line.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/Line.kt
@@ -4,8 +4,13 @@ import android.util.Log
 
 class Line(private val name:String) {
     private var list:MutableList<Mas> = mutableListOf()
-    private val comPiece = -1
+    private val comPiece=-1
     private val humanPiece = 1
+    private val empty = 0
+
+    private val bigPiece=3
+    private val middlePiece=2
+    private val smallPiece=1
 
     fun nameGetter():String{
         return name
@@ -39,6 +44,49 @@ class Line(private val name:String) {
             }
         }
         return counter
+    }
+
+    //同じライン上で自分の大きいコマを3つ使っているか?
+    fun use3BigPieceOnTheLine():Boolean{
+        //true :3つあった
+        //false:それ以外
+        var counter = 0
+        for (mas in list){
+            if (mas.funcForDisplay()[1] == comPiece && mas.funcForDisplay()[0] == bigPiece){ counter+=1 }
+            //マスに自分の大きいコマが入っていたら+1
+        }
+        if (counter == 3){return true}
+        return false
+    }
+
+    //同じライン上で自分の中コマを3つ使っているか?
+    fun use3MiddlePieceOnTheLine():Boolean{
+        //true :3つあった
+        //false:それ以外
+        var counter = 0
+        for (mas in list){
+            if (mas.funcForDisplay()[1] == comPiece && mas.funcForDisplay()[0] == middlePiece){
+                //マスに自分の中コマが入っていたら+1
+                counter+=1
+            }
+        }
+        if (counter == 3){return true}
+        return false
+    }
+
+    //同じライン上で自分の小コマを3つ使っているか?
+    fun use3SmallPieceOnTheLine():Boolean{
+        //true :3つあった
+        //false:それ以外
+        var counter = 0
+        for (mas in list){
+            if (mas.funcForDisplay()[1] == comPiece && mas.funcForDisplay()[0] == smallPiece){
+                //マスに自分の小さいコマが入っていたら+1
+                counter+=1
+            }
+        }
+        if (counter == 3){return true}
+        return false
     }
 
     fun deb(){
