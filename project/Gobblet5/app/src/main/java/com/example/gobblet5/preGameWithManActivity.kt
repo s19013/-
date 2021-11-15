3dccedffa20ab228f6f0aee3b2347cbf5f69888a
知念英弥 <s19013@std.it-college.ac.jp>
2021-11-15 16:44:40 +0900

    preGWMAクラスにベースを継承させた

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/preGameWithManActivity.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/preGameWithManActivity.kt
@@ -1,62 +1,20 @@
 package com.example.gobblet5
 
 import android.content.Intent
-import android.content.res.Resources
-import android.graphics.drawable.Drawable
-import android.media.AudioAttributes
-import android.media.SoundPool
 import android.os.Bundle
 import android.util.Log
-import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.widget.RadioButton
 import android.widget.RadioGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_pre_game_with_man.*
 
 
-class preGameWithManActivity : AppCompatActivity() {
-    private lateinit var sp: SoundPool
-    private var cancelSE = 0
-    private var gameStartSE = 0
-    private var radioButtonSE = 0
-
-    private var res: Resources? = null
-
+class preGameWithManActivity : BaseClass() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pre_game_with_man)
 
-        //共有プリファレンス
-        val pref = PreferenceManager.getDefaultSharedPreferences(this)
-        var SE =pref.getBoolean("SEOnOff", true)
-        var playFirst=pref.getInt("playFirst", 1)
-
-        val audioAttributes = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                .build()
-        } else { TODO("VERSION.SDK_INT < LOLLIPOP") }
-        sp = SoundPool.Builder()
-            .setAudioAttributes(audioAttributes)
-            .setMaxStreams(1)
-            .build()
-
-        cancelSE = sp.load(this, R.raw.cancel, 1)
-        gameStartSE = sp.load(this,R.raw.game_start_se,1)
-        radioButtonSE = sp.load(this,R.raw.radio_button,1)
-
-        fun playSound(status: Int){
-            if (SE){
-                when(status){
-                    cancelSE -> sp.play(cancelSE, 1.0f, 1.0f, 1, 0, 1.0f)
-                    radioButtonSE -> sp.play(radioButtonSE,1.0f,1.0f,1,0,1.0f)
-                    gameStartSE -> sp.play(gameStartSE, 1.0f, 1.0f, 1, 0, 1.0f)
-                }
-            }
-        }
+        var playFirst=pref?.getInt("playFirst", 1)
 
         val Radio = findViewById<RadioGroup>(R.id.RadioGroup)
         when(playFirst){
@@ -72,8 +30,8 @@ class preGameWithManActivity : AppCompatActivity() {
                 R.id.ButtonRandom->{playFirst=0}
             }
             playSound(radioButtonSE)
-            val editor=pref.edit()
-            editor.putInt("playFirst",playFirst).apply()
+            val editor=pref!!.edit()
+            editor.putInt("playFirst",playFirst!!).commit()
             Log.d("gobblet2", "${playFirst}")
         }
 
@@ -91,10 +49,5 @@ class preGameWithManActivity : AppCompatActivity() {
         }
 
 
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        sp.release()
     }
 }
