c30bdef0db733571ea9bac936a9a251e7f8659a7
知念英弥 <s19013@std.it-college.ac.jp>
2021-11-15 16:32:05 +0900

    mainクラスにベースクラスを継承させた

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/MainActivity.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/MainActivity.kt
@@ -14,16 +14,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
-    //音関係
-    private lateinit var sp: SoundPool
-    private var menuSelectSE = 0
-
+class MainActivity : BaseClass() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text =findViewById<View>(R.id.goPreGameWithComBtn)
+        iniall()
 
         val logo  = findViewById<ImageView>(R.id.logoImg)
 
@@ -34,35 +30,6 @@ class MainActivity : AppCompatActivity() {
         } else {
             logo.setImageResource(R.drawable.logo_en)
             Log.d("gobblet2", "en")
-        }
-
-
-        //共有プリファレンス
-        val pref = PreferenceManager.getDefaultSharedPreferences(this)
-        var SE =pref.getBoolean("SEOnOff", true)
-        var BGM =pref.getBoolean("BGMOnOff", true)
-
-
-        val audioAttributes = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                .build()
-        } else { TODO("VERSION.SDK_INT < LOLLIPOP") }
-        sp = SoundPool.Builder()
-            .setAudioAttributes(audioAttributes)
-            .setMaxStreams(1)
-            .build()
-
-        menuSelectSE = sp.load(this, R.raw.button, 1)
-
-        fun playSound(status: Int){
-            Log.d("gobblet2", "status:${status}")
-            if (SE){
-                when(status){
-                    menuSelectSE -> sp.play(menuSelectSE, 1.0f, 1.0f, 1, 0, 1.0f)
-                }
-            }
         }
 
 
@@ -95,7 +62,6 @@ class MainActivity : AppCompatActivity() {
     override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
         Log.d("gobblet2", "KeyEvent:${event}")
         val code = event?.keyCode
-
         return super.dispatchKeyEvent(event)
     }
 }
