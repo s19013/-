a923a226da8b45e56bd7dcd3c2d09c830260db63
知念英弥 <s19013@std.it-college.ac.jp>
2021-11-16 10:15:57 +0900

    #整理整頓 baseClass継承

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/preGameWithComActivity.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/preGameWithComActivity.kt
@@ -11,64 +11,16 @@ import android.widget.RadioGroup
 import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_pre_game_with_man.*
 
-class preGameWithComActivity : AppCompatActivity() {
-    private lateinit var sp: SoundPool
-    private var cancelSE = 0
-    private var radioButtonSE = 0
-    private var gameStartSE = 0
+class preGameWithComActivity : BaseClass() {
+    var radio:RadioGroup? =null
+    var playFirst:Int?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pre_game_with_com)
 
-        //共有プリファレンス
-        val pref = PreferenceManager.getDefaultSharedPreferences(this)
-        var SE =pref.getBoolean("SEOnOff", true)
-        var playFirst=pref.getInt("playFirst", 1)
-
-        val audioAttributes = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                .build()
-        } else { TODO("VERSION.SDK_INT < LOLLIPOP") }
-        sp = SoundPool.Builder()
-            .setAudioAttributes(audioAttributes)
-            .setMaxStreams(1)
-            .build()
-
-        cancelSE = sp.load(this, R.raw.cancel, 1)
-        gameStartSE = sp.load(this,R.raw.game_start_se,1)
-        radioButtonSE = sp.load(this,R.raw.radio_button,1)
-
-        fun playSound(status: Int){
-            if (SE){
-                when(status){
-                    cancelSE -> sp.play(cancelSE, 1.0f, 1.0f, 1, 0, 1.0f)
-                    radioButtonSE -> sp.play(radioButtonSE,1.0f,1.0f,1,0,1.0f)
-                    gameStartSE -> sp.play(gameStartSE, 1.0f, 1.0f, 1, 0, 1.0f)
-                }
-            }
-        }
-
-        val Radio = findViewById<RadioGroup>(R.id.RadioGroup)
-        when(playFirst){
-            1 -> {findViewById<RadioButton>(R.id.Button1p).isChecked = true}
-            -1 -> {findViewById<RadioButton>(R.id.Button2p).isChecked = true}
-            0 -> {findViewById<RadioButton>(R.id.ButtonRandom).isChecked = true}
-        }
-
-        Radio.setOnCheckedChangeListener { group, checkedId ->
-            when(checkedId){
-                R.id.Button1p->{playFirst=1}
-                R.id.Button2p->{playFirst= -1}
-                R.id.ButtonRandom->{playFirst=0}
-            }
-            playSound(radioButtonSE)
-            val editor=pref.edit()
-            editor.putInt("playFirst",playFirst).apply()
-            Log.d("gobblet2", "playFirst:${playFirst}")
-        }
+        iniPlayFirst()
+        iniRadioButtons()
 
         backButton.setOnClickListener {
             playSound(cancelSE)
@@ -84,8 +36,27 @@ class preGameWithComActivity : AppCompatActivity() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        sp.release()
+    fun iniPlayFirst(){
+        playFirst=pref?.getInt("playFirst", 1)
+        when(playFirst){
+            1 -> {findViewById<RadioButton>(R.id.Button1p).isChecked = true}
+            -1 -> {findViewById<RadioButton>(R.id.Button2p).isChecked = true}
+            0 -> {findViewById<RadioButton>(R.id.ButtonRandom).isChecked = true}
+        }
+    }
+
+    fun iniRadioButtons(){
+        radio = findViewById<RadioGroup>(R.id.RadioGroup)
+        radio!!.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.Button1p->{playFirst=1}
+                R.id.Button2p->{playFirst= -1}
+                R.id.ButtonRandom->{playFirst=0}
+            }
+            playSound(radioButtonSE)
+            val editor=pref!!.edit()
+            editor.putInt("playFirst",playFirst!!).apply()
+            Log.d("gobblet2", "${playFirst}")
+        }
     }
 }
