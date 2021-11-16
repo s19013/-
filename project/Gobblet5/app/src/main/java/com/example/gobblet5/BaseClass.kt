0cc6893dd40d0628a7876d88d064f1aa950d6bc5
知念英弥 <s19013@std.it-college.ac.jp>
2021-11-16 10:00:22 +0900

    #整頓整頓

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/BaseClass.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/BaseClass.kt
@@ -1,30 +1,41 @@
 package com.example.gobblet5
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.media.AudioAttributes
 import android.media.SoundPool
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+//11/16日にやることほとんどのクラスにbaseClassを継承させる
 
 open class BaseClass: AppCompatActivity()  {
-    
     //効果音
-    private lateinit var sp: SoundPool
-    private var cancelSE = 0
-    private var seekSE = 0
-    private var radioButtonSE = 0
-    private var menuSelectSE = 0
-    private var gameStartSE = 0
+    private   var sp: SoundPool?= null
+    protected var cancelSE = 0
+    protected var seekSE = 0
+    protected var radioButtonSE = 0
+    protected var menuSelectSE = 0
+    protected var gameStartSE = 0
 
 
     //プレファレンス
     protected var pref: SharedPreferences? =null
     protected var seVolume = 0
     protected var bgmVolume = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        iniall()
+    }
 
     protected fun iniall(){
         iniPreference()
@@ -51,16 +62,16 @@ open class BaseClass: AppCompatActivity()  {
             .setMaxStreams(1)
             .build()
         //使う効果音を準備
-        cancelSE = sp.load(this, R.raw.cancel, 1)
-        radioButtonSE = sp.load(this,R.raw.radio_button,1)
-        seekSE=sp.load(this,R.raw.select_se,1)
-        menuSelectSE = sp.load(this, R.raw.button, 1)
-        gameStartSE = sp.load(this,R.raw.game_start_se,1)
+        cancelSE = sp!!.load(this, R.raw.cancel, 1)
+        radioButtonSE = sp!!.load(this,R.raw.radio_button,1)
+        seekSE=sp!!.load(this,R.raw.select_se,1)
+        menuSelectSE = sp!!.load(this, R.raw.button, 1)
+        gameStartSE = sp!!.load(this,R.raw.game_start_se,1)
 
     }
 
     protected fun playSound(status: Int){
-        if (seVolume>0){ sp.play(status,seVolume*0.1f,seVolume*0.1f,1,0,1.0f) }
+        if (seVolume>0){ sp!!.play(status,seVolume*0.1f,seVolume*0.1f,1,0,1.0f) }
     }
 
     override fun onDestroy() {
