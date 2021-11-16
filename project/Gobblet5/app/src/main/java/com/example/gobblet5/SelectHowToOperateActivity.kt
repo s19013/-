88fc2240f4b6aa450fc5638fc13f1c73bf1d5512
知念英弥 <s19013@std.it-college.ac.jp>
2021-11-16 10:25:15 +0900

    baseクラスを継承

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/SelectHowToOperateActivity.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/SelectHowToOperateActivity.kt
@@ -10,46 +10,11 @@ import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_select_how_to_operate.*
 
 
-class SelectHowToOperateActivity : AppCompatActivity() {
-    //音関係
-    private lateinit var sp: SoundPool
-    private var cancelSE = 0
-    private var menuSelectSE = 0
+class SelectHowToOperateActivity : BaseClass() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_how_to_operate)
-
-        //共有プリファレンス
-        val pref = PreferenceManager.getDefaultSharedPreferences(this)
-        var SE =pref.getBoolean("SEOnOff", true)
-        var BGM =pref.getBoolean("BGMOnOff", true)
-
-
-        val audioAttributes = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                .build()
-        } else { TODO("VERSION.SDK_INT < LOLLIPOP") }
-        sp = SoundPool.Builder()
-            .setAudioAttributes(audioAttributes)
-            .setMaxStreams(1)
-            .build()
-
-        cancelSE = sp.load(this, R.raw.cancel, 1)
-        menuSelectSE = sp.load(this,R.raw.menu_selected,1)
-
-        fun playSound(status: Int){
-            Log.d("gobblet2", "status:${status}")
-            if (SE){
-                when(status){
-                    cancelSE -> sp.play(cancelSE, 1.0f, 1.0f, 1, 0, 1.0f)
-                    menuSelectSE -> sp.play(menuSelectSE, 1.0f, 1.0f, 1, 0, 1.0f)
-                }
-            }
-        }
-
 
         MovePieceFromHandToSquareButton.setOnClickListener {
             playSound(menuSelectSE)
