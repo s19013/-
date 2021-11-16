fec47be1ac3871f743688e9a2fd62aa024c908a0
知念英弥 <s19013@std.it-college.ac.jp>
2021-11-16 10:15:41 +0900

    #整理整頓

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/preGameWithManActivity.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/preGameWithManActivity.kt
@@ -9,31 +9,15 @@ import kotlinx.android.synthetic.main.activity_pre_game_with_man.*
 
 
 class preGameWithManActivity : BaseClass() {
+    var radio:RadioGroup? =null
+    var playFirst:Int?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pre_game_with_man)
 
-        var playFirst=pref?.getInt("playFirst", 1)
-
-        val Radio = findViewById<RadioGroup>(R.id.RadioGroup)
-        when(playFirst){
-            1 -> {findViewById<RadioButton>(R.id.Button1p).isChecked = true}
-            -1 -> {findViewById<RadioButton>(R.id.Button2p).isChecked = true}
-            0 -> {findViewById<RadioButton>(R.id.ButtonRandom).isChecked = true}
-        }
-
-        Radio.setOnCheckedChangeListener { group, checkedId ->
-            when(checkedId){
-                R.id.Button1p->{playFirst=1}
-                R.id.Button2p->{playFirst= -1}
-                R.id.ButtonRandom->{playFirst=0}
-            }
-            playSound(radioButtonSE)
-            val editor=pref!!.edit()
-            editor.putInt("playFirst",playFirst!!).apply()
-            Log.d("gobblet2", "${playFirst}")
-        }
+        iniPlayFirst()
+        iniRadioButtons()
 
         backButton.setOnClickListener {
             playSound(cancelSE)
@@ -48,6 +32,29 @@ class preGameWithManActivity : BaseClass() {
             startActivity(intent)
         }
 
+    }
 
+    fun iniPlayFirst(){
+        playFirst=pref?.getInt("playFirst", 1)
+        when(playFirst){
+            1 -> {findViewById<RadioButton>(R.id.Button1p).isChecked = true}
+            -1 -> {findViewById<RadioButton>(R.id.Button2p).isChecked = true}
+            0 -> {findViewById<RadioButton>(R.id.ButtonRandom).isChecked = true}
+        }
+    }
+    
+    fun iniRadioButtons(){
+        radio = findViewById<RadioGroup>(R.id.RadioGroup)
+        radio!!.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.Button1p->{playFirst=1}
+                R.id.Button2p->{playFirst= -1}
+                R.id.ButtonRandom->{playFirst=0}
+            }
+            playSound(radioButtonSE)
+            val editor=pref!!.edit()
+            editor.putInt("playFirst",playFirst!!).apply()
+            Log.d("gobblet2", "${playFirst}")
+        }
     }
 }
