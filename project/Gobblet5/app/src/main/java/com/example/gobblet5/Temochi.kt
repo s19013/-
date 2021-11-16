ea0220f2e552680a042363776bd28c051f9bd0be
知念英弥 <s19013@std.it-college.ac.jp>
2021-11-16 10:39:09 +0900

    整理整頓

--- project/Gobblet5/app/src/main/java/com/example/gobblet5/Temochi.kt
+++ project/Gobblet5/app/src/main/java/com/example/gobblet5/Temochi.kt
@@ -2,18 +2,14 @@ package com.example.gobblet5
 
 class Temochi(val size:Int,private val name:String) {
     private var count:Int=3
-    fun usePiece(){
-        count-=1
-    }
+
+    fun usePiece(){ count-=1 }
 
     fun plus(){
-        if (count<3)
-        count+=1
+        if (count<3){ count+=1 }
     }
 
-    fun nameGetter():String{
-        return name
-    }
+    fun nameGetter():String{ return name }
 
     fun returnInf():Int{
         if (count>0){
@@ -22,9 +18,7 @@ class Temochi(val size:Int,private val name:String) {
         return 0
     }
 
-    fun returnCount():Int{
-        return count
-    }
+    fun returnCount():Int{ return count }
 
 
 }
